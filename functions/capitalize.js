01da1cf198ba6ab5e2d718244e16e20194d4f704
Riyad <Riyad9876@gmail.com>
2023-09-08 12:33:51 -0400

    test, capitalize should return a string

--- functions/capitalize.js
+++ functions/capitalize.js
@@ -1,5 +1,9 @@
-function capitalize() {
-
+function capitalize(str) {
+    let firstLet = str[0]
+    firstLet = firstLet.toUpperCase()
+    return (firstLet + str.slice(1))
 }
  
+// console.log(capitalize('hello'));
+
 module.exports= capitalize
--- test/capitalize.test.js
+++ test/capitalize.test.js
@@ -3,7 +3,11 @@ const chai = require('chai')
 const expect = chai.expect
 
 describe('Tests for Capitalize function', () => {
-    it('Capitalize should be a function', ()=>{
+    it('capitalize should be a function', ()=>{
         expect(capitalize).to.be.a('function')
+    })
+
+    it('capitalize should return a string', ()=>{
+        expect(capitalize('hello')).to.be.a('string')
     })
 })
