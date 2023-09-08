8cb61a411b6b6f9623e557dd3bc046d4fd1ecefd
Riyad <Riyad9876@gmail.com>
2023-09-08 12:23:00 -0400

    check capitalize is a function

--- functions/capitalize.js
+++ functions/capitalize.js
@@ -1,5 +1,5 @@
 function capitalize() {
 
 }
-
-export default capitalize
+ 
+module.exports= capitalize
--- test/capitalize.test.js
+++ test/capitalize.test.js
@@ -1,1 +1,9 @@
 const capitalize = require('../functions/capitalize')
+const chai = require('chai')
+const expect = chai.expect
+
+describe('Tests for Capitalize function', () => {
+    it('Capitalize should be a function', ()=>{
+        expect(capitalize).to.be.a('function')
+    })
+})
